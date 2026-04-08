a44248adffaaaae06635c126ea9ba78591ed6c7b
shweta-kumaran <shweta.kumaran@gmail.com>
2026-04-07 19:15:53 -0500

    reorganize plots

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import ChoroplethMap from './components/ChoroplethMap'
+import BubbleScatterPlot from './components/BubbleScatterPlot'
 import './App.css'
 
 function App() {
   return (
     <main>
       <ChoroplethMap />
+      <BubbleScatterPlot />
     </main>
   )
 }
